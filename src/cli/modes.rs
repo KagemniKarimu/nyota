b241043647d36ecf2943c250ef1c7668544bf027
dariaag <dariaagad@gmail.com>
2025-01-29 17:02:16 +0200

    fix arg opts

--- src/cli/modes.rs
+++ src/cli/modes.rs
@@ -1,4 +1,4 @@
-use clap::{Arg,ArgGroup,ArgAction,  Command};
+use clap::{Arg, ArgAction, ArgGroup, Command};
 
 #[derive(Debug)]
 pub enum Mode {
@@ -15,24 +15,33 @@ pub struct ModeSettings {
 pub fn get_mode_input() -> ModeSettings {
     // Parse User Input Flags
     let flag_input = Command::new("nyota")
-        .arg(Arg::new("interactive")
-            .short('i')
-            .long("interactive")
-            .action(ArgAction::SetTrue)
-            .help("Start in interactive REPL mode"))
-        .arg(Arg::new("development")
-            .short('d')
-            .long("dev")
-            .action(ArgAction::SetTrue)
-            .help("Start in development mode with raw outputs"))
-        .arg(Arg::new("task")
-            .short('t')
-            .long("task")
-            .action(ArgAction::SetTrue)
-            .help("Execute a single task"))
-        .group(ArgGroup::new("modes")
-            .args(["interactive", "development", "task"])
-            .required(true))
+        .arg(
+            Arg::new("interactive")
+                .short('i')
+                .long("interactive")
+                .action(ArgAction::SetTrue)
+                .help("Start in interactive REPL mode"),
+        )
+        .arg(
+            Arg::new("development")
+                .short('d')
+                .long("dev")
+                .action(ArgAction::SetTrue)
+                .help("Start in development mode with raw outputs"),
+        )
+        .arg(
+            Arg::new("task")
+                .short('t')
+                .long("task")
+                .action(ArgAction::SetTrue)
+                .help("Execute a single task"),
+        )
+        .group(
+            ArgGroup::new("modes")
+                .args(["interactive", "development", "task"])
+                .required(false)
+                .multiple(false),
+        )
         .get_matches();
 
     // Debug Print of Flag Input
@@ -46,8 +55,9 @@ pub fn get_mode_input() -> ModeSettings {
             Mode::Development
         } else if flag_input.get_flag("task") {
             Mode::Task
-        } else { //if no Flag detected, default to Menu
+        } else {
+            //if no Flag detected, default to Menu
             Mode::Menu
-        }
+        },
     }
 }
