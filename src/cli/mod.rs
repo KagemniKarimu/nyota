2c08b2459af2132d5832ddbbfd3508c02bedacef
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2025-02-13 07:14:40 -0600

    📝 [cli] Add Module-Level Documentation for CLI

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,1 +1,5 @@
+//! # Command-Line Interface Module (`nyota::cli`)
+//! This module is the command-line interface module for `nyota`. This module contains the complete implementation of the command-line interface.
+//! It provides a minimal interface with a few modes/settings for user flags and options.
+
 pub mod modes;
