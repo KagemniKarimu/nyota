fa0462bf4d060b3c7b3cd7c32318a7bc279812f0
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2025-02-13 07:16:06 -0600

    📝 [lex] Add Module-Level Documentation for Lexer Module

---
+++ src/lex/mod.rs
@@ -0,0 +1,7 @@
+//! Lexer Module (`nyota::lex`)
+//! This module is the lexer module for `nyota`. This module contains the complete implementation of the lexer/parsing system.
+//! It provides functions for tokenizing and parsing input strings into a structured format.
+//! Pre-parsing is done here to ensure that the input is in a format that can be understood by relevant APIs.
+//! It also provides functions for recognizing macros, heuristics, and shortcuts from user input.
+
+pub mod lexer;
