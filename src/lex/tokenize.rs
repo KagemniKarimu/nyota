5de3cd9b18c9de42405d30accf25cc6ad0e00ee7
kagemnikarimu <82295340+KagemniKarimu@users.noreply.github.com>
2025-03-06 11:57:34 -0600

    ✨ [lex] Add Keyword Extraction Paradigm

---
+++ src/lex/tokenize.rs
@@ -0,0 +1,14 @@
+use yake_rust::{get_n_best, Config, ResultItem, StopWords};
+
+type KeyWordList = Vec<ResultItem>;
+
+fn extract_keywords(text: &str) -> KeyWordList {
+    let config = Config {
+        ngrams: 3,
+        ..Config::default()
+    };
+    let ignored = StopWords::predefined("en").unwrap();
+    let keywords = get_n_best(10, &text, &ignored, &config);
+
+    keywords
+}
